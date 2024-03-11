f8a5945f745cfcedde3203f5001973013063ee60
Salahuddin Hairai <mr.od3n@gmail.com>
2024-03-12 00:37:43 +0800

    Built-in types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-console.log('Hello World');
-let age: number  = 20;
-if (age < 50)
-  age += 10;
-console.log(age);
+let sales = 123456789;
+let course = 'TypeScript';
+let is_published = true;
+let level;
