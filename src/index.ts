d85aa382c43bfd915e0705928b1122c55f41f6d0
Salahuddin Hairai <mr.od3n@gmail.com>
2024-03-12 08:44:27 +0800

    Optional chaining

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
-function greet(name: string | null | undefined) {
-  if (name)
-    console.log(name.toUpperCase());
-  else
-    console.log('Hola!');
+type Customer = {
+  birthday: Date;
+};
+
+function getCustomer(id: number): Customer | null | undefined {
+  return id === 0 ? null : { birthday: new Date() };
 }
 
-greet(undefined);
+let customer = getCustomer(1);
+// Optional property access operator
+console.log(customer?.birthday?.getFullYear());
+
+// Optional element access operator
+// if (customers !==  null && customers !== undefined)
+// customers[0]
+// customers?.[0]
+
+// Optional call
+let log: any = null;
+log?.('a');
