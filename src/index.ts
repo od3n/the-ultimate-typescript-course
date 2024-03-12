11a7c13bd131dd1d31c2a9ff4092b69bb7523206
Salahuddin Hairai <mr.od3n@gmail.com>
2024-03-12 08:55:35 +0800

    The nullish coaelscing operator

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,5 @@
-type Customer = {
-  birthday: Date;
+let speed: number | null = null;
+let ride = {
+  // speed: speed !== null ? speed : 30,
+  speed: speed ?? 30,
 };
-
-function getCustomer(id: number): Customer | null | undefined {
-  return id === 0 ? null : { birthday: new Date() };
-}
-
-let customer = getCustomer(1);
-// Optional property access operator
-console.log(customer?.birthday?.getFullYear());
-
-// Optional element access operator
-// if (customers !==  null && customers !== undefined)
-// customers[0]
-// customers?.[0]
-
-// Optional call
-let log: any = null;
-log?.('a');
